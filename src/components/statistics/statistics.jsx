33fc43663cb92b7a5cd264f56b49d4984e6631f6
bartek1190 <bartlomiej.m.michonski@gmail.com>
2023-11-14 13:35:23 +0100

    Profile and Statistics

---
+++ src/components/statistics/statistics.jsx
@@ -0,0 +1,21 @@
+import css from './statistics.module.css';
+import { getRandomColor } from './randomColor';
+export const Statistics = ({ title, stats }) => {
+  return (
+    <section className={css.statistics}>
+      {title !== undefined && <h2 className={css.title}>{title}</h2>}
+      <ul className={css.statsList}>
+        {stats.map(stat => (
+          <li
+            key={stat.id}
+            className={css.item}
+            style={{ backgroundColor: `${getRandomColor()}` }}
+          >
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
